9d3748eb80681703b9f1fded4402322610979efc
r1v3r <churiver@outlook.com>
2022-08-16 15:25:18 +0800

    tests: fix example

--- example_test.go
+++ example_test.go
@@ -9,8 +9,35 @@ import (
 )
 
 func ExampleWorkerManager_NewInstance() {
-	fmt.Println("hello world")
-	// Output: hello world
+	mgr := wm.NewWorkerManager(context.Background())
+
+	mgr.RegisterWorker(DummyWorkerA, dummyBuilder)
+	mgr.RegisterWorker(DummyWorkerB, dummyBuilder)
+
+	mgr.RegisterStep(StepA, dummyStepRunner, dummyStepProcessor, StepB)
+	mgr.RegisterStep(StepB, dummyStepRunner, dummyStepProcessor)
+
+	mgr.Serve()
+
+	task := wm.NewTask(context.Background())
+	task.(*wm.Task).TaskToken = "example_token_123"
+	mgr.AddTask(task)
+
+	err := mgr.Recv(StepA, &dummyTarget{DummyTarget: wm.DummyTarget{TaskToken: task.Token()}, step: StepA})
+	if err != nil {
+		fmt.Printf("send target fail: %s", err)
+	}
+
+	for c := time.Tick(100 * time.Millisecond); !task.IsFinished(); <-c {
+	}
+
+	resultTask := mgr.GetTask(task.Token()).(*wm.Task)
+	fmt.Printf("got task: { token: %s, finished: %t }", resultTask.TaskToken, resultTask.Finished)
+
+	// Output:
+	// got result: &{DummyTarget:{TaskToken:example_token_123} step:step_b}
+	// got result: &{DummyTarget:{TaskToken:example_token_123} step:step_a}
+	// got task: { token: example_token_123, finished: true }
 }
 
 func ExampleWorkerManager_Singleton() {
