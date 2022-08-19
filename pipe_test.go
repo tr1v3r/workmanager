a828d1867fba317c358efad0dece2458494967ce
r1v3r <churiver@outlook.com>
2022-08-20 00:49:27 +0800

    tests: fix progress

--- pipe_test.go
+++ pipe_test.go
@@ -2,7 +2,6 @@ package workmanager
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 )
@@ -13,12 +12,12 @@ func TestPipeManager_recver(t *testing.T) {
 	mgr.RegisterWorker(DummyWorkerA, DummyBuilder)
 	mgr.RegisterWorker(DummyWorkerB, DummyBuilder)
 
+	var endGame bool
 	mgr.RegisterStep(StepA, DummyStepRunner, DummyStepProcessor, StepB)
-	mgr.RegisterStep(StepB, DummyStepRunner, DummyStepProcessor)
-
-	mgr.SetPipe(StepB, StepRecver(func(target WorkTarget) {
-		t.Logf("%s got target %+v, resending...", StepB, target)
-	}))
+	mgr.RegisterStep(StepB, func(_ Work, target WorkTarget, _ ...func(WorkTarget)) {
+		t.Logf("%s got target %+v, transfering...", StepB, target)
+		endGame = true
+	}, nil)
 
 	mgr.Serve()
 
@@ -28,13 +27,13 @@ func TestPipeManager_recver(t *testing.T) {
 
 	err := mgr.Recv(StepA, &DummyTestTarget{DummyTarget: DummyTarget{TaskToken: task.Token()}, Step: StepA})
 	if err != nil {
-		fmt.Printf("send target fail: %s", err)
+		t.Logf("send target fail: %s\n", err)
 	}
 
-	for c := time.Tick(100 * time.Millisecond); !task.IsFinished(); <-c {
-		fmt.Printf("task: %+v", task)
+	for c := time.Tick(100 * time.Millisecond); !endGame; <-c {
+		t.Logf("task: %+v\n", task)
 	}
 
 	resultTask := mgr.GetTask(task.Token()).(*Task)
-	fmt.Printf("got task: { token: %s, finished: %t }", resultTask.TaskToken, resultTask.Finished)
+	t.Logf("got task: { token: %s, finished: %t }\n", resultTask.TaskToken, resultTask.Finished)
 }
