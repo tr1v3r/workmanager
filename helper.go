5b1e182709a4fa42033e0ff2cb57d11da265e5c0
flight <32171650+ox1234@users.noreply.github.com>
2023-06-26 19:43:44 +0800

    feat(ctx): add step runner and callback context (#15)
    * feat(ctx): add step runner and callback context
    
    * feat: update transfer runner
    
    ---------
    
    Co-authored-by: r1v3r <churiver@outlook.com>

--- helper.go
+++ helper.go
@@ -1,8 +1,10 @@
 package workmanager
 
+import "context"
+
 var (
 	// TransferRunner runner for transfering
-	TransferRunner = func(trans func(WorkTarget)) StepRunner {
-		return func(_ Work, target WorkTarget, _ ...func(WorkTarget)) { trans(target) }
+	TransferRunner = func(trans func(context.Context, WorkTarget)) StepRunner {
+		return func(ctx context.Context, _ Work, target WorkTarget, _ ...func(WorkTarget)) { trans(ctx, target) }
 	}
 )
